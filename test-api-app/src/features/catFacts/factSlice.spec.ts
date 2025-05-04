01a0af453db6f861cea97975d579a5735a3c5c0c
Nicholas Mansfield <nickdmansfield@gmail.com>
2025-05-04 03:36:17 -0400

    Finished tests

--- test-api-app/src/features/catFacts/factSlice.spec.ts
+++ test-api-app/src/features/catFacts/factSlice.spec.ts
@@ -1,7 +1,11 @@
 import factReducer, {
   FactState,
-  selectFact
+  selectFact,
+  getCatFacts,
+  hideFact
 } from './factSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import * as api from './factAPI';
 
 describe('fact reducer', () => {
   const initialState: FactState = {
@@ -15,6 +19,7 @@ describe('fact reducer', () => {
     expect(factReducer(undefined, { type: 'unknown' })).toEqual({
       factList: initialState.factList,
       selectedFact: initialState.selectedFact,
+      hiddenFacts: [],
       errors: initialState.errors,
       status: initialState.status
     });
@@ -26,6 +31,7 @@ describe('fact reducer', () => {
     expect(results.selectedFact).toEqual('factorino 1');
   });
 
+  // clear selected fact
   it('should clear the selected fact when select is called with a null', () => {
     const testState = {...initialState, factList: ['factorino 1', 'facto 2'], selectedFact: 'facto 2'};
     const results = factReducer(testState, selectFact(null));
@@ -33,16 +39,32 @@ describe('fact reducer', () => {
   });
 
   // getCatFacts
-  it('should set the stored facts to the ones returned by the network call', () => {
-    // const testState = {...initialState, factList: ['factorino 1', 'facto 2'], selectedFact: 'facto 2'};
-    // const results = factReducer(testState, selectFact(null));
-    expect(true).toEqual(null);
+  it('should set the stored facts to the ones returned by the network call', async () => {
+    
+      // mock
+      jest.spyOn(api, 'fetchCatFacts').mockResolvedValueOnce({
+        data: ['Mock Fact 1', 'Mock Fact 2'],
+      });
+      // setup
+      const store = configureStore({
+        reducer: {
+          fact: factReducer,
+        },
+      });
+  
+      // execute
+      await store.dispatch(getCatFacts());
+  
+      // validate
+      const state = store.getState().fact;
+      expect(state.factList).toEqual(['Mock Fact 1', 'Mock Fact 2']);
+      expect(state.status).toBe('succeeded');
   });
 
   // hideFact
   it('should hide the clicked fact', () => {
-    // const testState = {...initialState, factList: ['factorino 1', 'facto 2'], selectedFact: 'facto 2'};
-    // const results = factReducer(testState, selectFact(null));
-    expect(true).toEqual(null);
+    const testState = {...initialState, factList: ['factorino 1', 'facto 2'] };
+    const resultState = factReducer(testState, hideFact('facto 2'));
+    expect(resultState.hiddenFacts).toEqual(['facto 2']);
   });
 });
