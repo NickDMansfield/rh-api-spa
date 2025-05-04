1d4971b4c91320eef33bddfb410a5e94def09648
Nicholas Mansfield <nickdmansfield@gmail.com>
2025-05-04 02:46:32 -0400

    Fixed issue with back button breaking the browser navigation.  Added browser styles

--- test-api-app/src/features/catFacts/factDetail.tsx
+++ test-api-app/src/features/catFacts/factDetail.tsx
@@ -12,16 +12,22 @@ export function FactDetail() {
   const selectedFact = useAppSelector(getSelectedFact);
   const dispatch = useAppDispatch();
 
+
+  function handleSelectFact(fact: string | null) {
+    window.history.pushState({ selectedFact: fact }, '', '');
+    dispatch(selectFact(fact));
+  }
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%', alignItems: 'center'}}>
-      <div className={styles.column}>
+      <div className={styles.centeredColumn}>
         <div>{selectedFact}</div>
-        <div> <a href={`http://google.com/search?q=${selectedFact?.replaceAll(' ', '%20')}`}
+        <div className={styles.learnMoreLink}> <a href={`http://google.com/search?q=${selectedFact?.replaceAll(' ', '%20')}`}
         target="_blank"
         rel="noopener noreferrer">
           Click here to learn more</a></div>
       </div>
-      <div className={styles.row} style={{display: 'flex', justifyContent:'flex-end', width: '100%'}} onClick={() => dispatch(selectFact(null))}>{'<'}<img style={{ height: '10vh' }} src={walkingCat} /></div>
+      <div className={styles.row} style={{display: 'flex', justifyContent:'flex-end', width: '100%', cursor: 'pointer'}} onClick={() => handleSelectFact(null)}>{'<'}<img style={{ height: '10vh' }} src={walkingCat} /></div>
     </div>
   );
 }
